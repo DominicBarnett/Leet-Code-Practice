128c1c5b5f4a0f31d22f71e820a839e2ca9288ce
DominicBarnett <dominic.barnett@icloud.com>
2025-01-13 10:12:12 -0800

    use properties of set to maximize efficiency

--- problemOne.js
+++ problemOne.js
@@ -33,20 +33,31 @@
 // }
 
 // learning about the set() function 
+// class Solution {
+//     /**
+//      * @param {number[]} nums
+//      * @return {boolean}
+//      */
+//     hasDuplicate(nums) {
+//         const seen = new Set();
+//         for (const num of nums) {
+//             if (seen.has(num)) {
+//                 return true;
+//             }
+//             seen.add(num);
+//         }
+//         return false;
+//     }
+// }
+
+// use properties of set to optimize once more
 class Solution {
     /**
      * @param {number[]} nums
      * @return {boolean}
      */
     hasDuplicate(nums) {
-        const seen = new Set();
-        for (const num of nums) {
-            if (seen.has(num)) {
-                return true;
-            }
-            seen.add(num);
-        }
-        return false;
+        return new Set(nums).size < nums.length;
     }
 }
 
