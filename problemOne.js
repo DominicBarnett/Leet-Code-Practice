dac7bedb89950476a9698f9419105e51e757a780
DominicBarnett <dominic.barnett@icloud.com>
2025-01-13 10:09:26 -0800

    learning about the set() function

--- problemOne.js
+++ problemOne.js
@@ -15,18 +15,36 @@
 
 // Output: false
 
+// class Solution {
+//     /**
+//      * @param {number[]} nums
+//      * @return {boolean}
+//      */
+//     hasDuplicate(nums) {
+//         for (let i = 0; i < nums.length; i++) {
+//             for (let j = i + 1; j < nums.length; j++) {
+//                 if (nums[i] === nums[j]) {
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+// learning about the set() function 
 class Solution {
     /**
      * @param {number[]} nums
      * @return {boolean}
      */
     hasDuplicate(nums) {
-        for (let i = 0; i < nums.length; i++) {
-            for (let j = i + 1; j < nums.length; j++) {
-                if (nums[i] === nums[j]) {
-                    return true;
-                }
+        const seen = new Set();
+        for (const num of nums) {
+            if (seen.has(num)) {
+                return true;
             }
+            seen.add(num);
         }
         return false;
     }
