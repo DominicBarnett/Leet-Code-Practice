03bec0b072bcc93c8bfcf0715a9f8b9360ca8104
DominicBarnett <dominic.barnett@icloud.com>
2025-01-13 09:59:30 -0800

    Brute force problem 1

---
+++ problemOne.js
@@ -0,0 +1,41 @@
+// Problem 1
+
+// Contains Duplicate
+// Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+// Example 1:
+
+// Input: nums = [1, 2, 3, 3]
+
+// Output: true
+
+// Example 2:
+
+// Input: nums = [1, 2, 3, 4]
+
+// Output: false
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = i + 1; j < nums.length; j++) {
+                if (nums[i] === nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+const solution = new Solution();
+
+const test = [1, 2, 3, 3];
+const test2 = [1, 2, 3, 4];
+
+console.log(solution.hasDuplicate(test));  // Output: true
+console.log(solution.hasDuplicate(test2)); // Output: false
