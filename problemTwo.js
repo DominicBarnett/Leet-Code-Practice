5a9d681f2906d23b10c72ae65cac7b5edb0899b0
DominicBarnett <dominic.barnett@icloud.com>
2025-01-14 10:48:28 -0800

    hashtable solution

--- problemTwo.js
+++ problemTwo.js
@@ -17,6 +17,24 @@
 // s and t consist of lowercase English letters.
 
 
+// class Solution {
+//     /**
+//      * @param {string} s
+//      * @param {string} t
+//      * @return {boolean}
+//      */
+//     isAnagram(s, t) {
+//         if (s.length !== t.length) {
+//             return false;
+//         }
+
+//         let sSort = s.split("").sort().join();
+//         let tSort = t.split("").sort().join();
+//         return sSort == tSort
+//     }
+// }
+
+// Hashtable solution
 class Solution {
     /**
      * @param {string} s
@@ -24,12 +42,28 @@ class Solution {
      * @return {boolean}
      */
     isAnagram(s, t) {
+        // if the lengths aren't the same then they aren't anagrams
         if (s.length !== t.length) {
             return false;
         }
 
-        let sSort = s.split("").sort().join();
-        let tSort = t.split("").sort().join();
-        return sSort == tSort
+        // initialize dictionaries
+        const countS = {};
+        const countT = {};
+        // since the length of the strings are the same we can count from either one
+        for (let i = 0; i < s.length; i++) {
+            // for the string s take the character at index i and make that a key in the dictionary set the value of that key to the amount of times it appears in the string
+            // at the same time do the same process for the second string
+            countS[s[i]] = (countS[s[i]] || 0) + 1;
+            countT[t[i]] = (countT[t[i]] || 0) + 1;
+        }
+
+        // check if the keys and counts match in both dictionaries
+        for (const key in countS) {
+            if (countS[key] !== countT[key]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
